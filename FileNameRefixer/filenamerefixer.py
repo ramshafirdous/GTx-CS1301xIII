f4c79d3ca7272446051772b3ecd09d0346d77adc
ramshafirdous <104720378+ramshafirdous@users.noreply.github.com>
2022-05-06 14:02:00 +0800

    Create filenamerefixer.py

---
+++ FileNameRefixer/filenamerefixer.py
@@ -0,0 +1,62 @@
+#You've been sent a list of names. Unfortunately, the names
+#come in two different formats:
+#
+#First Middle Last
+#Last, First Middle
+#
+#You want the entire list to be the same. For this problem,
+#we'll say you want the entire list to be Last, First Middle.
+#
+#Write a function called name_refixer. name_refixer should have two
+#parameters: an output filename (the first parameter) and the
+#input filename (the second parameter). You may assume that every
+#line will match one of the two formats above: either First Middle
+#Last or Last, First Middle. 
+#
+#name_refixer should write to the output file the names all
+#structured as Last, First Middle. If the name was already structured
+#as Last, First Middle, it should remain unchanged. If it was
+#structured as First Middle Last, then Last should be moved
+#to the front and a comma should be added after it.
+#
+#The names should appear in the same order as the original file.
+#
+#For example, if the input file contained the following lines:
+#David Andrew Joyner
+#Hart, Melissa Joan
+#Cyrus, Billy Ray
+#
+#...then the output file should contain these lines:
+#Joyner, David Andrew
+#Hart, Melissa Joan
+#Cyrus, Billy Ray
+
+
+#Add your code here!
+def name_refixer(output_file, input_file):
+    with open(input_file) as f, open(output_file,"w") as output:
+        for line in f:
+            names= line.split()  
+            if ',' in names[0]:
+                output.write(names[0]+" "+names[1]+" "+names[2]+"\n")
+            else:
+                output.write(names[2]+","+" "+names[0]+" "+names[1]+"\n")
+
+
+
+#The code below will test your function. You can find the two
+#files it references in the drop-down in the top left. If your
+#code works, output_file.txt should have the text:
+#Joyner, David Andrew
+#Hart, Melissa Joan
+#Cyrus, Billy Ray
+name_refixer("output_file.txt", "input_file.txt")
+print("Done running! Check output_file.txt for the result.")
+
+#If you accidentally erase input_file.txt, here's its original
+#text to copy back in (remove the pound signs):
+#David Andrew Joyner
+#Hart, Melissa Joan
+#Cyrus, Billy Ray
+
+
